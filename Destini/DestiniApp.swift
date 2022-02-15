f2707c913626cab37edc0c01a67f73bc59612829
Vladimir Fibe <motiw@icloud.com>
2022-02-15 09:07:51 +0600

    Initial Commit

---
+++ Destini/DestiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestiniApp.swift
+//  Destini
+//
+//  Created by Vladimir Fibe on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DestiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
