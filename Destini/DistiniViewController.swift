41208981e7dcb7daf8ed44ffdfc6af136c509240
Vladimir Fibe <motiw@me.com>
2022-04-06 06:43:02 +0600

    small changes

--- Destini/DistiniViewController.swift
+++ Destini/DistiniViewController.swift
@@ -8,70 +8,68 @@
 import UIKit
 
 class DistiniViewController: UIViewController {
-
   var storyBrain = StoryBrain()
-  let backgroundView = UIImageView(image: UIImage(named: "background"))
+  let backgroundView: UIImageView = {
+    $0.contentMode = .scaleAspectFill
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    return $0
+  }(UIImageView(image: UIImage(named: "background")))
+  
   let storyLabel: UILabel = {
-    let label = UILabel()
-    label.text = "Story Text"
-    label.font = .systemFont(ofSize: 25)
-    label.textColor = .white
-    label.numberOfLines = 0
-    return label
-  }()
-  let choice1Button: UIButton = {
-    let button = UIButton(type: .system)
-    button.setTitle("Choice 1", for: .normal)
-    button.titleLabel?.font = .systemFont(ofSize: 23)
-    button.layer.cornerRadius = 16
-    button.backgroundColor = .red
-    button.titleLabel?.lineBreakMode = .byWordWrapping
-    button.titleLabel?.textAlignment = .center
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.heightAnchor.constraint(equalToConstant: 100).isActive = true
-    button.addTarget(self, action: #selector(choiceMade), for: .touchUpInside)
-    return button
-  }()
-  let choice2Button: UIButton = {
-    let button = UIButton(type: .system)
-    button.setTitle("Choice 2", for: .normal)
-    button.titleLabel?.font = .systemFont(ofSize: 23)
-    button.layer.cornerRadius = 16
-    button.backgroundColor = .purple
-    button.titleLabel?.lineBreakMode = .byWordWrapping
-    button.titleLabel?.textAlignment = .center
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.heightAnchor.constraint(equalToConstant: 100).isActive = true
-    button.addTarget(self, action: #selector(choiceMade), for: .touchUpInside)
-    return button
-  }()
-  lazy var buttons = [choice1Button, choice2Button]
+    $0.text = "Story Text"
+    $0.font = .systemFont(ofSize: 25)
+    $0.adjustsFontSizeToFitWidth = true
+    $0.numberOfLines = 0
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    return $0
+  }(UILabel())
+  
+  let buttons = [UIButton(type: .system), UIButton(type: .system)]
   func setupUI() {
-    view.addSubview(backgroundView)
-    backgroundView.contentMode = .scaleAspectFill
-    backgroundView.translatesAutoresizingMaskIntoConstraints = false
-    backgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-    backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-    backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    let stack = UIStackView(arrangedSubviews: [storyLabel, choice1Button, choice2Button])
-    stack.axis = .vertical
-    stack.distribution = .fillProportionally
-    stack.alignment = .fill
-    stack.spacing = 20
-    view.addSubview(stack)
+    buttons.forEach {
+      $0.titleLabel?.font = .systemFont(ofSize: 23)
+      $0.titleLabel?.adjustsFontSizeToFitWidth = true
+      $0.layer.cornerRadius = 16
+      $0.backgroundColor = .purple
+      $0.titleLabel?.lineBreakMode = .byWordWrapping
+      $0.titleLabel?.textAlignment = .center
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      $0.addTarget(nil, action: #selector(choiceMade), for: .touchUpInside)
+    }
+    buttons[0].backgroundColor = .red
+
     let margins = view.layoutMarginsGuide
+    let stack = UIStackView(arrangedSubviews: buttons)
+    stack.axis = .vertical
+    stack.distribution = .fillEqually
+    stack.spacing = 10
     stack.translatesAutoresizingMaskIntoConstraints = false
-    stack.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-    stack.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
-    stack.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-    stack.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+    
+    view.addSubview(backgroundView)
+    view.addSubview(stack)
+    view.addSubview(storyLabel)
+    
+    NSLayoutConstraint.activate([
+      backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+      backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -1),
+      backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+      stack.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -20),
+      stack.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+      stack.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+      stack.heightAnchor.constraint(equalToConstant: 190),
+      
+      storyLabel.topAnchor.constraint(equalTo: margins.topAnchor),
+      storyLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+      storyLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+      storyLabel.bottomAnchor.constraint(equalTo: stack.topAnchor, constant: -20)
+    ])
   }
   override func viewDidLoad() {
     super.viewDidLoad()
     setupUI()
     updateUI()
-
   }
   @objc func choiceMade(_ sender: UIButton) {
     if let title = sender.currentTitle {
